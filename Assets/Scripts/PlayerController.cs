9dcbe56ff498372e73c96f43a7c1d77461616bcb
Anjack <gary.reif@gmail.com>
2018-04-24 16:18:56 -0400

    Physics changes.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,27 +14,27 @@ public class PlayerController : MonoBehaviour {
     private float yaw;
     private float pitch;
 
-    private KeyCode sprint_key;
-
-    CharacterController my_controller;
+    private Vector3 move_range;
 
     Animator body_animator;
+    Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-        my_controller = gameObject.GetComponent<CharacterController>();
 
         body_animator = body.GetComponent<Animator>();
 
-        // Temporary config section (need to move to independent or player perfs)
-        sprint_key = KeyCode.LeftShift;
+        rb = gameObject.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float delta_h = Input.GetAxis("Horizontal");
-        float delta_v = Input.GetAxis("Vertical");
 
+        RaycastHit hit_info = new RaycastHit();
+
+        gameObject.GetComponent<Collider>().Raycast(new Ray(transform.position, transform.TransformDirection(Vector3.down)), out hit_info, 10.0f);
+
+        Debug.Log(hit_info.distance);
 
         // Set camera rotation based upon mouse look (but only when right mouse button is held down)
         if (Input.GetMouseButton(1))
@@ -47,24 +47,28 @@ public class PlayerController : MonoBehaviour {
 
         }
 
-        // Move set character speed and move them
-        //Vector3 new_move = new Vector3(move_h, 0, move_v) * speed * Time.deltaTime;
-
+        // Set character speed and move them
         float adjusted_speed = speed;
 
-        // Sprint key
-        if(Input.GetKey(sprint_key))
-        {
-            adjusted_speed = speed * 2;
-        }
+        float delta_right = Input.GetAxis("Horizontal");
+        float delta_forward = Input.GetAxis("Vertical");
 
-        Vector3 move_h = transform.right * delta_h * adjusted_speed * Time.deltaTime;
-        Vector3 move_v = transform.forward * delta_v * adjusted_speed * Time.deltaTime;
+        Vector3 new_move = new Vector3(delta_right, 0, delta_forward) * adjusted_speed * Time.deltaTime;
 
-        Vector3 new_move = move_h + move_v;
+        // To prevent "skiing"
 
-        my_controller.Move(new_move);
+        float observed_speed = new_move.magnitude / Time.deltaTime;
 
-        body_animator.SetFloat("Speed", my_controller.velocity.magnitude);
+        Debug.Log(observed_speed);
+
+        // Move us to the new position (need collision check)
+        transform.Translate(new_move);
+
+        body_animator.SetFloat("Speed", observed_speed);
 	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
 }

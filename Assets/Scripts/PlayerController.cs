002db90d3862778dfa253fc50061baa9be3c417c
Anjack <gary.reif@gmail.com>
2018-04-26 15:40:48 -0400

    Fixed movement and animations.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,70 +5,33 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float speed;
-    public float rotation_speed;
 
     public GameObject body;
 
-    public Camera player_cam;
-
-    private float yaw;
-    private float pitch;
-
-    private Vector3 move_range;
+    CharacterController my_controller;
 
     Animator body_animator;
-    Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
+        my_controller = gameObject.GetComponent<CharacterController>();
 
         body_animator = body.GetComponent<Animator>();
-
-        rb = gameObject.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        float move_h = Input.GetAxis("Horizontal");
+        float move_v = Input.GetAxis("Vertical");
 
-        RaycastHit hit_info = new RaycastHit();
+        //transform.Translate(move_h * Time.deltaTime, 0, move_v * Time.deltaTime);
 
-        gameObject.GetComponent<Collider>().Raycast(new Ray(transform.position, transform.TransformDirection(Vector3.down)), out hit_info, 10.0f);
+        Vector3 new_move = new Vector3(move_h, 0, move_v) * speed * Time.deltaTime;
 
-        Debug.Log(hit_info.distance);
+        my_controller.Move(new_move);
 
-        // Set camera rotation based upon mouse look (but only when right mouse button is held down)
-        if (Input.GetMouseButton(1))
-        {
-            yaw += Input.GetAxis("Mouse X") * rotation_speed * Time.deltaTime;
-            pitch += Input.GetAxis("Mouse Y") * rotation_speed * Time.deltaTime;
+        Debug.Log(my_controller.velocity.magnitude);
 
-            transform.eulerAngles = new Vector3(transform.rotation.x, transform.rotation.y + yaw, 0.0f);
-            player_cam.transform.eulerAngles = new Vector3(player_cam.transform.rotation.x - pitch, player_cam.transform.rotation.y + yaw, 0.0f);
-
-        }
-
-        // Set character speed and move them
-        float adjusted_speed = speed;
-
-        float delta_right = Input.GetAxis("Horizontal");
-        float delta_forward = Input.GetAxis("Vertical");
-
-        Vector3 new_move = new Vector3(delta_right, 0, delta_forward) * adjusted_speed * Time.deltaTime;
-
-        // To prevent "skiing"
-
-        float observed_speed = new_move.magnitude / Time.deltaTime;
-
-        Debug.Log(observed_speed);
-
-        // Move us to the new position (need collision check)
-        transform.Translate(new_move);
-
-        body_animator.SetFloat("Speed", observed_speed);
+        body_animator.SetFloat("Speed", my_controller.velocity.magnitude);
 	}
-
-    private void OnCollisionEnter(Collision collision)
-    {
-
-    }
 }

020f71a413745224e14da9d2aeb2832c7ee84622
Anjack <gary.reif@gmail.com>
2018-04-29 09:41:18 -0400

    Added pistol to character. Added bullet for pistol.

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+    public float ballistic_force;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+
+        rb.AddForce(Vector3.forward * ballistic_force, ForceMode.Force);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
